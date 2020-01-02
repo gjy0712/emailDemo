465cfe378b22069b2eb8399d3542d98265672bfc
Jinyan.Ge <imgjyan@163.com>
2020-01-02 11:01:15 +0800

    react-table

--- src/pages/email/email.js
+++ src/pages/email/email.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './comment.css';
+import './email.less';
 import { Layout, Menu } from 'element-react'
 
 import EmailTable from './email-table/email-table'
@@ -22,17 +22,22 @@ class Email extends Component {
                     <Layout.Col span={8}>
                         <h5>Cloud Email</h5>
                         <Menu defaultActive="2" className="el-menu-vertical-demo" onOpen={this.onOpen.bind(this)} onClose={this.onClose.bind(this)}>
-                            <Menu.SubMenu index="1" title={<span><i className="el-icon-message"></i>导航一</span>}>
-                                <Menu.ItemGroup title="分组一">
-                                    <Menu.Item index="1-1">选项1</Menu.Item>
-                                    <Menu.Item index="1-2">选项2</Menu.Item>
-                                </Menu.ItemGroup>
-                                <Menu.ItemGroup title="分组2">
-                                    <Menu.Item index="1-3">选项3</Menu.Item>
+                            <Menu.SubMenu index="1" title={<span><i className="el-icon-star-on"></i>收藏夹</span>}>
+                                <Menu.ItemGroup>
+                                    <Menu.Item index="1-1">收件箱</Menu.Item>
+                                    <Menu.Item index="1-2">已发送邮件</Menu.Item>
+                                    <Menu.Item index="1-3">已删除邮件</Menu.Item>
                                 </Menu.ItemGroup>
                             </Menu.SubMenu>
-                            <Menu.Item index="2"><i className="el-icon-menu"></i>导航二</Menu.Item>
-                            <Menu.Item index="3"><i className="el-icon-setting"></i>导航三</Menu.Item>
+                            <Menu.SubMenu index="2" title={<span><i className="el-icon-menu"></i>所有分类</span>}>
+                                <Menu.ItemGroup>
+                                    <Menu.Item index="2-1">收件箱</Menu.Item>
+                                    <Menu.Item index="2-2">草稿箱</Menu.Item>
+                                    <Menu.Item index="2-3">已发送邮件</Menu.Item>
+                                    <Menu.Item index="2-4">发件箱</Menu.Item>
+                                    <Menu.Item index="2-5">垃圾邮件</Menu.Item>
+                                </Menu.ItemGroup>
+                            </Menu.SubMenu>
                         </Menu>
                     </Layout.Col>
                 </Layout.Row>
